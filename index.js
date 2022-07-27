be51dbeb9139106c9842e6ea9e0f6b35ded7a373
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2022-07-27 12:30:59 +0530

    feat:added sample btn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var inputTxt=document.querySelector("#inputText");
 var btnTranslate=document.querySelector("#button")
 var outputTxt=document.querySelector("#outputText")
+var sampleBtn=document.querySelector("#exp-btn")
 
 var serverURL="https://api.funtranslations.com/translate/uk2us.json"
 
@@ -19,9 +20,23 @@ function constructURL(text){
             var outputTEXT = json.contents.translated
            outputTxt.innerText = outputTEXT
      })
+        .catch(errorHandler)
 
 
     }
     
+function eventHandleri(){
+
+    inputTxt.innerText="The artefacts of the behavioural elements cancelled the commercialised monotone"
+}
+
+function errorHandler(error){
+
+    console.log("error occured" ,error)
+    alert("Something went wrong in the server.Try again after some time")
+
+}
+
+var sampleBtn=addEventListener("dblclick" , eventHandleri)    
 var btnTranslate=addEventListener("click" , eventHandler)
 
